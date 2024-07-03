56d56d691fb965a8d3d66de945f7eebf9805f32c
DaliaGamal11 <119794418+DaliaGamal11@users.noreply.github.com>
2024-07-03 21:16:23 +0300

    Update profileTab.dart

--- lib/Tabs/profileTab.dart
+++ lib/Tabs/profileTab.dart
@@ -1,111 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
 class ProfileTab extends StatefulWidget {
-  const ProfileTab({super.key});
+  const ProfileTab({Key? key}) : super(key: key);
 
   @override
   _ProfileTabState createState() => _ProfileTabState();
 }
 
 class _ProfileTabState extends State<ProfileTab> {
-  final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
-  final _storage = FirebaseStorage.instance;
-  final _picker = ImagePicker();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _bioController = TextEditingController();
 
-  User? _currentUser;
-  String _username = '';
-  String _profileImageUrl = '';
-
-  late TextEditingController _nameController;
-  late TextEditingController _emailController;
-  late TextEditingController _addressController;
-  late TextEditingController _bioController;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentUser = _auth.currentUser;
-    _username = _currentUser?.email ?? ''; // Assuming username is the email
-    _nameController = TextEditingController();
-    _emailController = TextEditingController();
-    _addressController = TextEditingController();
-    _bioController = TextEditingController();
-
-    if (_currentUser != null) {
-      _loadUserProfile();
-    }
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _emailController.dispose();
-    _addressController.dispose();
-    _bioController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _loadUserProfile() async {
-    try {
-      final userDoc = await _firestore.collection('userProfiles').doc(_username).get();
-      if (userDoc.exists) {
-        setState(() {
-          _nameController.text = userDoc['name'];
-          _emailController.text = userDoc['email'];
-          _addressController.text = userDoc['address'];
-          _bioController.text = userDoc['bio'];
-          _profileImageUrl = userDoc['profileImageUrl'];
-        });
-        print('User profile loaded');
-      } else {
-        print('User profile document does not exist');
-      }
-    } catch (e) {
-      print('Error loading user profile: $e');
-    }
-  }
-
-  Future<void> _updateUserProfile() async {
-    if (_username.isEmpty) {
-      print('Username is empty. Cannot update profile.');
+  void _saveProfileData() {
+    // Check if any of the required fields are empty
+    if (_nameController.text.isEmpty ||
+        _emailController.text.isEmpty ||
+        _addressController.text.isEmpty ||
+        _bioController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please fill out all fields')),
+      );
       return;
     }
-    try {
-      await _firestore.collection('userProfiles').doc(_username).set({
-        'name': _nameController.text,
-        'email': _emailController.text,
-        'address': _addressController.text,
-        'bio': _bioController.text,
-        'profileImageUrl': _profileImageUrl,
-      });
-      print('User profile updated');
-    } catch (e) {
-      print('Error updating user profile: $e');
-    }
-  }
 
-  Future<void> _pickImage() async {
-    try {
-      final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-      if (pickedFile != null) {
-        File imageFile = File(pickedFile.path);
-        UploadTask uploadTask = _storage.ref('profile_images/$_username').putFile(imageFile);
-        final snapshot = await uploadTask.whenComplete(() => {});
-        final downloadUrl = await snapshot.ref.getDownloadURL();
-        setState(() {
-          _profileImageUrl = downloadUrl;
-        });
-        _updateUserProfile();
-      }
-    } catch (e) {
-      print('Error picking image: $e');
-    }
+    // Create a document with email as the document ID
+    FirebaseFirestore.instance
+        .collection('profiles')
+        .doc(_emailController.text) // Use email as document ID
+        .set({
+      'name': _nameController.text,
+      'email': _emailController.text,
+      'address': _addressController.text,
+      'bio': _bioController.text,
+    })
+        .then((value) => {
+      // Success
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Profile saved successfully')),
+      ),
+      // Clear text fields
+      _nameController.clear(),
+      _emailController.clear(),
+      _addressController.clear(),
+      _bioController.clear(),
+    })
+        .catchError((error) => {
+      // Error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to save profile')),
+      ),
+    });
   }
 
   @override
@@ -113,103 +60,59 @@ class _ProfileTabState extends State<ProfileTab> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Profile',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Profile',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
               ),
-              SizedBox(height: 20),
-              Center(
-                child: GestureDetector(
-                  onTap: _pickImage,
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: _profileImageUrl.isNotEmpty
-                        ? NetworkImage(_profileImageUrl)
-                        : AssetImage('assets/images/profile.png') as ImageProvider,
-                  ),
-                ),
+            ),
+            SizedBox(height: 10),
+            _buildTextField(_nameController, 'Name', Icons.person),
+            SizedBox(height: 10),
+            _buildTextField(_emailController, 'Email', Icons.email),
+            SizedBox(height: 10),
+            _buildTextField(_addressController, 'Address', Icons.home),
+            SizedBox(height: 10),
+            _buildTextField(_bioController, 'Bio', Icons.info),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _saveProfileData,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromRGBO(72, 132, 151, 1)),
               ),
-              SizedBox(height: 10),
-              Text(
-                "Name",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              child: Text(
+                'Save Profile',
+                style: TextStyle(color: Colors.white), // Text color
               ),
-              SizedBox(height: 5),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your name',
-                  prefixIcon: Icon(Icons.person),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                style: TextStyle(fontSize: 16),
-                controller: _nameController,
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Email",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your email',
-                  prefixIcon: Icon(Icons.email),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                style: TextStyle(fontSize: 16),
-                controller: _emailController,
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Address",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your address',
-                  prefixIcon: Icon(Icons.home),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                style: TextStyle(fontSize: 16),
-                controller: _addressController,
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Bio",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 5),
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your Bio',
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                style: TextStyle(fontSize: 16),
-                controller: _bioController,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _updateUserProfile,
-                child: Text('Save Profile'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField(
+      TextEditingController controller, String label, IconData icon) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Enter your $label',
+        prefixIcon: Icon(icon),
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      style: TextStyle(fontSize: 16),
     );
   }
 }
